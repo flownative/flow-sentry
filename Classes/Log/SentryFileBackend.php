flownative/flow-sentry

commit e9117897edccf777c29cbf8bde40a4607fb2d63a

Some code simplification

Classes/Log/SentryFileBackend.php
@@ -21,10 +21,7 @@ class SentryFileBackend extends FileBackend
 {
     use SentryClientTrait;
 
-    /**
-     * @var bool
-     */
-    private $capturingMessage = false;
+    private bool $capturingMessage = false;
 
     /**
      * Appends the given message along with the additional information into the log.
@@ -32,13 +29,12 @@ class SentryFileBackend extends FileBackend
      * @param string $message The message to log
      * @param int $severity One of the LOG_* constants
      * @param mixed $additionalData A variable containing more information about the event to be logged
-     * @param string|null $packageKey Key of the package triggering the log (determined automatically if not specified)
-     * @param string|null $className Name of the class triggering the log (determined automatically if not specified)
-     * @param string|null $methodName Name of the method triggering the log (determined automatically if not specified)
+     * @param string|null $packageKey Key of the package triggering the log
+     * @param string|null $className Name of the class triggering the log
+     * @param string|null $methodName Name of the method triggering the log
      * @return void
-     * @api
      */
-    public function append(string $message, int $severity = LOG_INFO, $additionalData = null, string $packageKey = null, string $className = null, string $methodName = null): void
+    public function append(string $message, int $severity = LOG_INFO, $additionalData = null, ?string $packageKey = null, ?string $className = null, ?string $methodName = null): void
     {
         if ($this->capturingMessage) {
             return;
@@ -49,20 +45,12 @@ class SentryFileBackend extends FileBackend
 
             $sentryClient = self::getSentryClient();
             if ($severity <= LOG_NOTICE && $sentryClient) {
-                switch ($severity) {
-                    case LOG_WARNING:
-                        $sentrySeverity = Severity::warning();
-                    break;
-                    case LOG_ERR:
-                        $sentrySeverity = Severity::error();
-                    break;
-                    case LOG_CRIT:
-                    case LOG_ALERT:
-                    case LOG_EMERG:
-                        $sentrySeverity = Severity::fatal();
-                    break;
-                    default:
-                        $sentrySeverity = Severity::info();
+                $sentrySeverity = match ($severity) {
+                    LOG_WARNING => Severity::warning(),
+                    LOG_ERR => Severity::error(),
+                    LOG_CRIT, LOG_ALERT, LOG_EMERG => Severity::fatal(),
+                    default => Severity::info(),
+                };
 
                 }
 

Classes/Command/SentryCommandController.php
@@ -14,18 +14,17 @@ namespace Flownative\Sentry\Command;
  */
 
 use Flownative\Sentry\SentryClient;
-use Flownative\Sentry\Test\JsonSerializableTestArgument;
 use Flownative\Sentry\Test\SentryClientTestException;
 use Flownative\Sentry\Test\StringableTestArgument;
 use Flownative\Sentry\Test\ThrowingClass;
-use Neos\Flow\Annotations\Inject;
+use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Cli\CommandController;
 use Sentry\Severity;
 
 final class SentryCommandController extends CommandController
 {
     /**
-     * @Inject
+     * @Flow\Inject
      * @var SentryClient
      */
     protected $sentryClient;

Classes/Context/DefaultUserContextService.php
@@ -18,10 +18,6 @@ use Neos\Flow\Security\Context;
 
 class DefaultUserContextService implements UserContextServiceInterface
 {
-    /**
-     * @param Context $securityContext
-     * @return UserContextInterface
-     */
     public function getUserContext(Context $securityContext): UserContextInterface
     {
         $userContext = new UserContext();

Classes/Context/UserContext.php
@@ -15,64 +15,37 @@ namespace Flownative\Sentry\Context;
 
 class UserContext implements UserContextInterface
 {
-    /**
-     * @var string
-     */
-    private $id = '';
+    private string $id = '';
 
-    /**
-     * @var string
-     */
-    private $username = '';
+    private string $username = '';
 
-    /**
-     * @var string
-     */
-    private $email = '';
+    private string $email = '';
 
-    /**
-     * @return string
-     */
     public function getId(): string
     {
         return $this->id;
     }
 
-    /**
-     * @param string $id
-     */
     public function setId(string $id): void
     {
         $this->id = $id;
     }
 
-    /**
-     * @return string
-     */
     public function getUsername(): string
     {
         return $this->username;
     }
 
-    /**
-     * @param string $username
-     */
     public function setUsername(string $username): void
     {
         $this->username = $username;
     }
 
-    /**
-     * @return string
-     */
     public function getEmail(): string
     {
         return $this->email;
     }
 
-    /**
-     * @param string $email
-     */
     public function setEmail(string $email): void
     {
         $this->email = $email;

Classes/Context/UserContextInterface.php
@@ -15,19 +15,10 @@ namespace Flownative\Sentry\Context;
 
 interface UserContextInterface
 {
-    /**
-     * @return string|null
-     */
     public function getId(): ?string;
 
-    /**
-     * @return string|null
-     */
     public function getUsername(): ?string;
 
-    /**
-     * @return string|null
-     */
     public function getEmail(): ?string;
 
     /**
@@ -36,8 +27,6 @@ interface UserContextInterface
      * "id", "username", "email"
      *
      * The keys must exist, but the values may be empty
-     *
-     * @return array
      */
     public function toArray(): array;
 }

Classes/Context/UserContextServiceInterface.php
@@ -19,9 +19,6 @@ interface UserContextServiceInterface
 {
     /**
      * Returns ContextData to be added to the sentry entry
-     *
-     * @param Context $securityContext
-     * @return UserContextInterface
      */
     public function getUserContext(Context $securityContext): UserContextInterface;
 }

Classes/SentryClientTrait.php
@@ -19,9 +19,6 @@ use Neos\Flow\ObjectManagement\ObjectManagerInterface;
 
 trait SentryClientTrait
 {
-    /**
-     * @return SentryClient|null
-     */
     protected static function getSentryClient(): ?SentryClient
     {
         if (!Bootstrap::$staticObjectManager instanceof ObjectManagerInterface || Bootstrap::$staticObjectManager instanceof CompileTimeObjectManager) {

commit f1fa5a295fdf1387dd6f3b4f596bd44461fe2f5a

Fix error introduced by e9117897edccf777c29cbf8bde40a4607fb2d63a

Classes/Log/SentryFileBackend.php
@@ -52,8 +52,6 @@ class SentryFileBackend extends FileBackend
                     default => Severity::info(),
                 };
 
-                }
-
                 $sentryClient->captureMessage($message, $sentrySeverity, ['Additional Data' => $additionalData]);
             }
             parent::append($message, $severity, $additionalData, $packageKey, $className, $methodName);